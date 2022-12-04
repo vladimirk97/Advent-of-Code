00576bbf796f74345820e45d91e7cba4f33f7c46
Vladimir Kostic <vladimir.kostic1997@gmail.com>
2022-12-04 19:03:55 +0100

    2022 day4 part 1

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,12 +1,23 @@
 def part1(input_lines):
-       
+    cnt = 0
+    for line in input_lines:
+        first_elf, second_elf = line.split(',')        
+        range1 = list(map(int,first_elf.split('-')))
+        range2 = list(map(int,second_elf.split('-')))
+        if range1[0]>=range2[0] and range1[1]<=range2[1] or range2[0]>=range1[0] and range2[1]<=range1[1]: cnt += 1
+
+    print(cnt)
+
+        
+        
+
 
 
 # def part2(input_lines):
  
     
 
-input_lines = open('Input/day3.txt').readlines()
+input_lines = open('Input/day4.txt').readlines()
 
 # Run
 part1(input_lines)
