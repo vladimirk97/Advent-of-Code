fc21ef08bc739444bb82bad4295c6f9d62c56d0f
Vladimir Kostic <vladimir@easyaerial.com>
2022-12-14 18:34:19 +0100

    Start 2022 day11

--- 2022/day11.py
+++ 2022/day11.py
@@ -1,14 +1,34 @@
-class CathodeRayTube():
+class Monkey():
 
-  def __init__(self):
-    self.name = 0
+  def __init__(self, monkey_index, starting_items, operation, operation_val, test, cond_true, cond_false):
+    self.monkey_index = int(monkey_index)
+    self.items = starting_items
+    self.operation = operation
+    self.operation_val = operation_val
+    self.test = int(test)
+    self.cond_true = int(cond_true)
+    self.cond_false = int(cond_false)
+
+  def 
 
 
 
 
-input_lines = open('Input/day10.txt').readlines()
+input_lines = open('Input/test.txt').readlines()
 
-for line in input_lines:
-  computer.read_instruction(line)
+monkeys = set()
 
-print(computer.signal_sum)
+for line in range(len(input_lines)):
+  if "Monkey" in input_lines[line]:
+    _, index = input_lines[line].replace(":", "").split()
+    items = list(map(int, input_lines[line+1][18:].strip().split(', ')))
+    _, op, op_val = input_lines[line+2][19:].strip().split(' ')
+    test = input_lines[line+3][21:].strip()
+    cond_true = input_lines[line+4][29:].strip()
+    cond_false = input_lines[line+5][30:].strip()
+
+    monkeys.add(Monkey(index, items, op, op_val, test, cond_true, cond_false))
+
+for _ in range(20):
+
+
